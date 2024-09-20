750fd6b25115a8dfb111695bb7ce27957db3fb00
Lê Hải <125958137+haile0205@users.noreply.github.com>
2024-09-21 01:06:47 +0700

    Init for EX5

---
+++ LAB1/Core/Inc/Ex5.h
@@ -0,0 +1,26 @@
+/*
+ * Ex5.h
+ *
+ *  Created on: Sep 12, 2024
+ *      Author: ADMIN
+ */
+
+#ifndef INC_EX5_H_
+#define INC_EX5_H_
+
+#define SEG_A_PIN SEG_0_Pin
+#define SEG_B_PIN SEG_1_Pin
+#define SEG_C_PIN SEG_2_Pin
+#define SEG_D_PIN SEG_3_Pin
+#define SEG_E_PIN SEG_4_Pin
+#define SEG_F_PIN SEG_5_Pin
+#define SEG_G_PIN SEG_6_Pin
+
+
+//extern void displaySeg7(int digit, int ledSet);
+//
+//extern void displayCountdown(int timeLeft);
+
+void Ex5();
+
+#endif /* INC_EX5_H_ */
